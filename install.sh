6b3b91a7c39e4732dead2eb347334bbbccc55532
EastAPOLO <eastapolo@hotmail.com>
2018-03-25 16:37:13 -0500

    Add gtk2 theme, improve gtk3 notebook and update install.sh

--- install.sh
+++ install.sh
@@ -24,6 +24,14 @@ cp -v "./gtk3/gtk.css" "$TARGET/gtk-3.0/gtk.css"
 cp -v "./gtk3/thumbnail.png" "$TARGET/gtk-3.0/thumbnail.png"
 cp -rv "./gtk3/assets/" "$TARGET/gtk-3.0/assets"
 
+# Copy gtk2 files to the gtk2 folder
+cp -v "./gtk2/gtkrc" "$TARGET/gtk-2.0/gtkrc"
+cp -v "./gtk2/apps.rc" "$TARGET/gtk-2.0/apps.rc"
+cp -v "./gtk2/xfce-notify.rc" "$TARGET/gtk-2.0/xfce-notify.rc"
+cp -v "./gtk2/main.rc" "$TARGET/gtk-2.0/main.rc"
+cp -rv "./gtk2/assets" "$TARGET/gtk-2.0/assets"
+cp -rv "./gtk2/menubar-toolbar" "$TARGET/gtk-2.0/menubar-toolbar"
+
 # Copy metacity files to metacity folder
 cp -v "./metacity/metacity-theme-2.xml" "$TARGET/metacity-1/"
 cp -v "./metacity/metacity-theme-3.xml" "$TARGET/metacity-1/"
