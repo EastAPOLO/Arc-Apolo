90a1c7c6462a8cfb5d7635a70414f6645b7b808c
Stefan Stojanovic <eastapolo@hotmail.com>
2018-06-02 12:32:54 -0500

    Require the script to ask for the password with sudo

--- install.sh
+++ install.sh
@@ -9,32 +9,32 @@ if [[ "$NAME" == "" ]]; then
 fi
 
 # Remove old directories
-rm -rfv "$TARGET"
+sudo rm -rfv "$TARGET"
 
 # Create required directories
-mkdir -pv "$TARGET/gtk-3.0"
-mkdir -pv "$TARGET/gtk-2.0"
-mkdir -pv "$TARGET/metacity-1"
-mkdir -pv "$TARGET/cinnamon"
+sudo mkdir -pv "$TARGET/gtk-3.0"
+sudo mkdir -pv "$TARGET/gtk-2.0"
+sudo mkdir -pv "$TARGET/metacity-1"
+sudo mkdir -pv "$TARGET/cinnamon"
 
 # Move theme info to the root theme dir
-cp -v "./index.theme" "$TARGET/index.theme"
+sudo cp -v "./index.theme" "$TARGET/index.theme"
 
 # Copy gtk3 files to the gtk3 folder
-cp -v "./gtk3/gtk.css" "$TARGET/gtk-3.0/gtk.css"
-cp -v "./gtk3/thumbnail.png" "$TARGET/gtk-3.0/thumbnail.png"
-cp -rv "./gtk3/assets/" "$TARGET/gtk-3.0/assets"
+sudo cp -v "./gtk3/gtk.css" "$TARGET/gtk-3.0/gtk.css"
+sudo cp -v "./gtk3/thumbnail.png" "$TARGET/gtk-3.0/thumbnail.png"
+sudo cp -rv "./gtk3/assets/" "$TARGET/gtk-3.0/assets"
 
 # Copy gtk2 files to the gtk2 folder
-cp -v "./gtk2/gtkrc" "$TARGET/gtk-2.0/gtkrc"
-cp -v "./gtk2/apps.rc" "$TARGET/gtk-2.0/apps.rc"
-cp -v "./gtk2/xfce-notify.rc" "$TARGET/gtk-2.0/xfce-notify.rc"
-cp -v "./gtk2/main.rc" "$TARGET/gtk-2.0/main.rc"
-cp -v "./gtk2/panel.rc" "$TARGET/gtk-2.0/panel.rc"
-cp -rv "./gtk2/assets" "$TARGET/gtk-2.0/assets"
-cp -rv "./gtk2/menubar-toolbar" "$TARGET/gtk-2.0/menubar-toolbar"
+sudo cp -v "./gtk2/gtkrc" "$TARGET/gtk-2.0/gtkrc"
+sudo cp -v "./gtk2/apps.rc" "$TARGET/gtk-2.0/apps.rc"
+sudo cp -v "./gtk2/xfce-notify.rc" "$TARGET/gtk-2.0/xfce-notify.rc"
+sudo cp -v "./gtk2/main.rc" "$TARGET/gtk-2.0/main.rc"
+sudo cp -v "./gtk2/panel.rc" "$TARGET/gtk-2.0/panel.rc"
+sudo cp -rv "./gtk2/assets" "$TARGET/gtk-2.0/assets"
+sudo cp -rv "./gtk2/menubar-toolbar" "$TARGET/gtk-2.0/menubar-toolbar"
 
 # Copy metacity files to metacity folder
-cp -v "./metacity/metacity-theme-2.xml" "$TARGET/metacity-1/"
-cp -v "./metacity/metacity-theme-3.xml" "$TARGET/metacity-1/"
-cp -rv "./metacity/assets/." "$TARGET/metacity-1/"
+sudo cp -v "./metacity/metacity-theme-2.xml" "$TARGET/metacity-1/"
+sudo cp -v "./metacity/metacity-theme-3.xml" "$TARGET/metacity-1/"
+sudo cp -rv "./metacity/assets/." "$TARGET/metacity-1/"
